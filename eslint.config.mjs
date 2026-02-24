6c138e2924d3a14d3e4738fe30b5a4141a88584e
Hamed Manrique <hmr.xyz@outlook.com>
2026-02-23 20:19:56 -0600

    fix: solved lint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
-import eslintConfigPrettier from 'eslint-config-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
